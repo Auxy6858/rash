e1080ed4b53cbd64d62e5c31ee281fdfe3590002
Cam <cambee093@gmail.com>
2025-03-07 16:36:39 +0000

    update number 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::{env, io::{self, Write}};
+use std::{env, fs, io::{self, Write}, path::Path, process::{Command, Stdio}};
+use std::process;
 use cmd::cmd_mode;
+
+use utils::{change_directory, get_current_directory};
 #[cfg(target_os = "windows")]
 use winapi::um::{processenv::GetStdHandle, wincon::{FillConsoleOutputAttribute, FillConsoleOutputCharacterA, GetConsoleScreenBufferInfo, SetConsoleCursorPosition, CONSOLE_SCREEN_BUFFER_INFO, COORD}};
 #[cfg(target_os = "windows")]
@@ -18,7 +21,8 @@ fn main() {
         if !username.is_empty() & !devicename.is_empty() {
             print!("\x1b[35m{username}\x1b[37m@\x1b[33m{devicename} \x1b[37m");
         }
-        print!("rash - {VERSION_NUMBER}> ");
+        print!("Rash {VERSION_NUMBER}\n");
+        print!("{}> ", get_current_directory());
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
@@ -29,7 +33,34 @@ fn main() {
 
         let trimmed_input = input.trim();
 
-        if trimmed_input.starts_with("echo") {
+        if trimmed_input.starts_with("cd") {
+            if trimmed_input == "cd" {
+                clear_screen();
+                println!("Not implemented yet");
+            } else if trimmed_input == "cd .." {
+                let mut new_directory = get_current_directory();
+                
+            }
+            if !trimmed_input.contains('/') {
+                let folder = trimmed_input.strip_prefix("cd").unwrap().trim();
+                let path_as_string = get_current_directory() + "/" + folder;
+                let path = Path::new(&path_as_string);
+                if path.exists() && path.is_dir() {
+                    change_directory(&path_as_string);
+                } else {
+                    println!("Invalid directory {}", path_as_string);
+                }
+            }
+        } else if trimmed_input.starts_with("./") {
+            let exetutable_name = trimmed_input.strip_prefix("./");
+            let process = Command::new(trimmed_input)
+                .stdin(Stdio::inherit())
+                .stdout(Stdio::inherit())
+                .stderr(Stdio::inherit())
+                .spawn()
+                .expect("failed to start");
+
+        } else if trimmed_input.starts_with("echo") {
             if !input.contains('%') {
                 println!("{}", input.strip_prefix("echo").unwrap().trim());
             }
@@ -40,31 +71,7 @@ fn main() {
                 "help" => println!("Available commands: exit, help, cd"),
                 "hello" => println!("Hi! :D"),
                 "version" => println!("\x1b[33mRash\x1b[37m version {VERSION_NUMBER}"),
-                "clear" => {
-                    if cfg!(target_os = "windows") {
-                        #[cfg(target_os = "windows")]
-                        unsafe {
-                            let handle: HANDLE = GetStdHandle(STD_OUTPUT_HANDLE);
-                            if handle.is_null() {
-                                eprintln!("Unable to get standard handle");
-                            }
-                            let mut screen_buffer_info: CONSOLE_SCREEN_BUFFER_INFO = std::mem::zeroed();
-                            if GetConsoleScreenBufferInfo(handle, &mut screen_buffer_info) == 0 {
-                                eprintln!("Unable to get console screen buffer info");
-                            }
-                            let screen_size: u32 = screen_buffer_info.dwSize.X as u32 * screen_buffer_info.dwSize.Y as u32;
-                            let mut chars_written: u32 = 0;
-                            let origin: COORD = COORD {X: 0, Y: 0};
-    
-                            FillConsoleOutputCharacterA(handle, ' ' as i8, screen_size, origin, &mut chars_written);
-                            FillConsoleOutputAttribute(handle, screen_buffer_info.wAttributes, screen_size, origin, &mut chars_written);
-                            SetConsoleCursorPosition(handle, origin);
-                        }
-                    } else {
-                        print!("\x1B[2J\x1B[1;1H");
-                        io::stdout().flush().unwrap();
-                    }
-                }
+                "clear" => clear_screen(),
                 "whereami" => {
                     println!("Your current directory is '{}'", utils::get_current_directory());
                 },
@@ -76,3 +83,28 @@ fn main() {
     }
 }
 
+fn clear_screen() {
+    if cfg!(target_os = "windows") {
+        #[cfg(target_os = "windows")]
+        unsafe {
+            let handle: HANDLE = GetStdHandle(STD_OUTPUT_HANDLE);
+            if handle.is_null() {
+                eprintln!("Unable to get standard handle");
+            }
+            let mut screen_buffer_info: CONSOLE_SCREEN_BUFFER_INFO = std::mem::zeroed();
+            if GetConsoleScreenBufferInfo(handle, &mut screen_buffer_info) == 0 {
+                eprintln!("Unable to get console screen buffer info");
+            }
+            let screen_size: u32 = screen_buffer_info.dwSize.X as u32 * screen_buffer_info.dwSize.Y as u32;
+            let mut chars_written: u32 = 0;
+            let origin: COORD = COORD {X: 0, Y: 0};
+
+            FillConsoleOutputCharacterA(handle, ' ' as i8, screen_size, origin, &mut chars_written);
+            FillConsoleOutputAttribute(handle, screen_buffer_info.wAttributes, screen_size, origin, &mut chars_written);
+            SetConsoleCursorPosition(handle, origin);
+        }
+    } else {
+        print!("\x1B[2J\x1B[1;1H");
+        io::stdout().flush().unwrap();
+    }
+}
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,16 @@
-use std::env;
+use std::{env, ffi::CString};
 
 pub fn get_current_directory() -> String {
     match env::current_dir() {
         Ok(path) => path.display().to_string(),
         Err(_) => String::from("Failed to get current directory"),
+    }
+}
+
+pub fn change_directory(path: &str) {
+    let path_as_cstring = CString::new(path).expect("Conversion to CString failed");
+    
+    if unsafe {libc::chdir(path_as_cstring.as_ptr())} != 0 {
+        eprintln!("Failed to change directory");
     }
 }
